302ecba544f081782c15bf6ae99d01b66c901443
Brent Anderson <brent@knock.app>
2023-04-04 11:13:40 -0500

    chore(kno-3385): test to verify idempotency key header works

--- tests/Unit/Api/WorkflowsTest.php
+++ tests/Unit/Api/WorkflowsTest.php
@@ -20,6 +20,22 @@ class WorkflowsTest extends ApiTest
 
         $this->assertEquals($expected, $workflows->trigger($key, []));
     }
+    
+    /** @test */
+    public function will_trigger_workflow_with_idempotency_key()
+    {
+        $key = 'new-comment';
+        $expected = $this->getContent(sprintf('%s/data/responses/workflow-trigger.json', __DIR__));
+        $headers = ['Idempotency-Key' => '12345'];
+
+        $workflows = $this->getApiMock();
+        $workflows->expects($this->once())
+            ->method('postRequest')
+            ->with(sprintf('/workflows/%s/trigger', $key), [], $headers)
+            ->will($this->returnValue($expected));
+
+        $this->assertEquals($expected, $workflows->trigger($key, [], $headers));
+    }
 
     /** @test */
     public function will_cancel_workflow()
